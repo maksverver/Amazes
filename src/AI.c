44183084eca7c3a38a62bc7768461fd82095832d
Maks Verver <maksverver@geocities.com>
2009-08-18 00:33:57 +0200

    - Implement simple sudden-death strategy to AI player.

--- src/AI.c
+++ src/AI.c
@@ -1,17 +1,15 @@
 #include "Analysis.h"
+#include <assert.h>
 
 #define MAX_TURNS 150
 
-const char *pick_move(MazeMap *mm, int distsq)
+static int dist[HEIGHT][WIDTH];
+
+static Point explore(MazeMap *mm)
 {
+    Point res;
     int r, c, dir;
-    int dist[HEIGHT][WIDTH];
-    int best_r = 0, best_c = 0, best_v = 0, best_dist = 0;
-
-    (void)distsq; /* unused */
-
-    find_distance(mm, dist, mm->loc.r, mm->loc.c);
-
+    int best_v = 0, best_dist = 0;
     for (r = 0; r < HEIGHT; ++r)
     {
         for (c = 0; c < WIDTH; ++c)
@@ -28,15 +26,62 @@ const char *pick_move(MazeMap *mm, int distsq)
                 {
                     best_v = v;
                     best_dist = dist[r][c];
-                    best_r = r;
-                    best_c = c;
+                    res.r = r;
+                    res.c = c;
                 }
             }
         }
     }
+    assert(best_v > 0);
+    return res;
+}
 
-    if (best_v == 0) return "T";
+static Point squash(MazeMap *mm, int distsq)
+{
+    Point res;
+    int r, c, best_dist = -1;
 
-    return construct_turn(mm, dist, mm->loc.r, mm->loc.c, mm->dir,
-                          best_r, best_c);
+    if (distsq <= 0) return mm->loc;
+
+    for (r = 0; r < HEIGHT; ++r)
+    {
+        for (c = 0; c < WIDTH; ++c)
+        {
+            const int dr = (r - mm->border.top + HEIGHT)%HEIGHT -
+                           (mm->loc.r - mm->border.top + HEIGHT)%HEIGHT,
+                      dc = (c - mm->border.left + WIDTH)%WIDTH -
+                           (mm->loc.c - mm->border.left + WIDTH)%WIDTH;
+
+            if (dr*dr + dc*dc == distsq)
+            {
+                int r = (mm->loc.r + dr + HEIGHT)%HEIGHT,
+                    c = (mm->loc.c + dc + WIDTH)%WIDTH;
+                if (best_dist == -1 || dist[r][c] < best_dist)
+                {
+                    best_dist = dist[r][c];
+                    res.r = r;
+                    res.c = c;
+                }
+            }
+        }
+    }
+    assert(best_dist != -1);
+    return res;
+}
+
+const char *pick_move(MazeMap *mm, int distsq)
+{
+    Point dst;
+
+    find_distance(mm, dist, mm->loc.r, mm->loc.c);
+
+    if (mm_count_squares(mm) < WIDTH*HEIGHT)
+        dst = explore(mm);
+    else /* mm_count_squares(mm) == WIDTH*HEIGHT */
+        dst = squash(mm, distsq);
+
+    if (dst.r == mm->loc.r && dst.c == mm->loc.c)
+        return "T";
+
+    return construct_turn(mm, dist, mm->loc.r, mm->loc.c, mm->dir, dst.r, dst.c);
 }
